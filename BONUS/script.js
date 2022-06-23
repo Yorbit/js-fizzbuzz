a8d81517d3e26fcaa8088a1c80adb61221fde5c3
Yorbit <marco.95112@hotmail.it>
2022-06-23 15:04:38 +0200

    gold not work

--- BONUS/script.js
+++ BONUS/script.js
@@ -17,16 +17,31 @@ for (let index = 1; index <= 100; index++) {
 
         console.log('f-b');
 
+        boxN.classList.add('box-n', 'box-fb');
+
+        boxN.innerText = 'fb';
+
+        bFRow.append(boxN);
 
     } else if (index % 5 == 0) {
 
         console.log('b');
 
+        boxN.classList.add('box-n', 'box-b');
+
+        boxN.innerText = 'b';
+
+        bFRow.append(boxN);
 
     } else if (index % 3 == 0) {
 
         console.log('f');
 
+        boxN.classList.add('box-n', 'box-f');
+
+        boxN.innerText = 'f';
+
+        bFRow.append(boxN);
 
     } else {
 
